4685fb36ea820a15c399242ed27bd29122548072
DrkBotBase <marziosincro@gmail.com>
2023-11-02 20:17:24 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+const axios = require('axios');
 const app = require('./app');
+
+setInterval(() => {
+  axios.get(`${Config.DOMINIO}/ping`).then(response => {
+    if (response.ok) {
+      console.log('Ping enviado para mantener la actividad.');
+    } else {
+      console.error('Error al enviar el ping.');
+    }
+  });
+}, 4 * 60 * 1000);
 
 async function main() {
     await app.listen(app.get('port'));
